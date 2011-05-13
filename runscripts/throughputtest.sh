10463e5e4b34700aaad5696ced0671cc347aeffc
Christian Spann <christian.spann@uni-ulm.de>
2011-05-13 11:42:40 +0200

    adjusted loglevel and parameters for better quality in testing results

--- runscripts/throughputtest.sh
+++ runscripts/throughputtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-measurement_int=100
-measurement_count=10
+measurement_int=1000
+measurement_count=10 # currently unused
 logdir=log
 cd $3
 javacmd="java -server -XX:CompileThreshold=0 -ea -Xmx65m -XX:+HeapDumpOnOutOfMemoryError -Dnavigators.smart.ebawa.configfile=config/ebawa.config -cp dist/modular_SMaRt.jar:lib/netty-3.1.1.GA.jar:lib/ConfigHandler.jar:lib/commons-math-2.2.jar -Djava.util.logging.config.file=config/logging.properties navigators.smart.tom.demo.ThroughputLatencyTestServer $1 $measurement_int $measurement_count"
