8ad6829dfd2947f6d0064c6cb56cad2f0d548b1f
Christian Spann <christian.spann@uni-ulm.de>
2011-05-25 16:45:19 +0200

    Added toString() to NettyClientServerSession.java

--- src/navigators/smart/communication/client/netty/NettyClientServerSession.java
+++ src/navigators/smart/communication/client/netty/NettyClientServerSession.java
@@ -83,4 +83,8 @@ public class NettyClientServerSession {
         this.lastMsgReceived = lastMsgReceived_;
     }
 
+    @Override
+    public String toString() {
+        return "Session: "+replicaId+" lastMsg: "+lastMsgReceived;
+    }
 }
