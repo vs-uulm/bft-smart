f27c128abbab46cf8cfe311492d60f1e9a1d54a8
Christian Spann <christian.spann@uni-ulm.de>
2013-07-12 17:05:34 +0200

    Changed copyright to apache 2.0

--- src/test/java/navigators/smart/tom/core/messages/TOMMessageTest.java
+++ src/test/java/navigators/smart/tom/core/messages/TOMMessageTest.java
@@ -13,7 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License along with SMaRt. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package navigators.smart.tom.core.messages;
 
 import static org.junit.Assert.assertEquals;
--- src/test/java/navigators/smart/tom/util/BatchBuilderReaderTest.java
+++ src/test/java/navigators/smart/tom/util/BatchBuilderReaderTest.java
@@ -13,7 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License along with SMaRt. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package navigators.smart.tom.util;
 
 import java.nio.ByteBuffer;
